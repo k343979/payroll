c0f524e4d278cd63c7e2929f51026f197593c8af
k343979 <yusuke040989@gmail.com>
2021-01-25 14:59:34 +0900

    upload

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // DBの文字数の設定
+        // Herokuでvarchar型の文字数はデフォルトの255では大きいため、191に設定
+        Schema::defaultStringLength(191);
+
+        // アプリをhttpsに対応
+        // 本番環境の場合にhttpsに強制
+        if (\App::environment('production')) {
+            \URL::forceSchema('https');
+        }
     }
 }
